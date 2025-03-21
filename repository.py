06be353624fe4b94ec4935c96eb188601a57c386
Scott Chamberlain <sachamber@fredhutch.org>
2025-03-21 10:56:57 -0700

    clean up repository.py

--- repository.py
+++ repository.py
@@ -1,30 +1,24 @@
-# from pygit2 import Repository
 from ghapi.all import GhApi
-from registry import WildsRegistry
-from rich import print
 from rich.console import Console
-console = Console(force_terminal=True)
 
+from registry import WildsRegistry
+
+console = Console(force_terminal=True)
 api = GhApi()
 reg = WildsRegistry()
-# repo = reg.all[0]
-# repo_gh = api.repos.get("getwilds", repo["name"])
-# repo_gh['default_branch']
 
 def check_for_main(repo):
+  """Returns 0 if rule satisfied, 1 if not satisfied"""
   repo_gh = api.repos.get("getwilds", repo["name"])
   default = repo_gh['default_branch']
   if "main" != default:
     console.print(f'[bold red]{repo["name"]}[/bold red] needs a main branch as default; instead got {default}')
-    # raise Exception(f'[bold red]{repo["name"]}[/bold red] needs a main branch as default; instead got {default}')
+    return 1
   else:
     console.print(f"[bold green]{repo["name"]}[/bold green] all good")
-  # repo = Repository(".")
-  # if "mains" not in list(repo.branches.local):
-  #   raise Exception(f'main branch not found for {repo}')
+    return 0
 
 if __name__ == "__main__":
-  [check_for_main(repo) for repo in reg.all]
-
-#   repo = Repository(".")
-#   check_for_main(repo)
+  out = [check_for_main(repo) for repo in reg.all]
+  if sum(out) > 0:
+    raise Exception("at least 1 repo did not satisfy rule, see above")
